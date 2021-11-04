a1e701044ada2b544e02261f0a0ab6df0e333291
MinisX <daniel.dedoukh@gmail.com>
2021-11-04 21:31:58 +0100

    version2

--- fileio/example3/Program.cs
+++ fileio/example3/Program.cs
@@ -12,12 +12,14 @@ class Program
     {
         string fname = @"test.txt";
         // StreamReader reads file line by line
+        // using checks if the object goes out of scope and if it does, then it called Dispose method, in which the file is closed
         using (StreamReader rd = File.OpenText(fname))
         {
             // While not at the end of the stream, read the next line
             while (!rd.EndOfStream)
             {
                 string line = rd.ReadLine();
+                // output the line
                 Console.WriteLine(line);
             }
         }

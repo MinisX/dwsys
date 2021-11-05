4d4f55cbc859796f75d074012cd49ffe19509d22
MinisX <daniel.dedoukh@gmail.com>
2021-11-05 16:06:53 +0100

    regex2

--- regex/Program.cs
+++ regex/Program.cs
@@ -11,7 +11,9 @@ namespace regex
         static void Main(string[] args)
         {
             String[] lines = File.ReadAllLines(filename);
-            Regex re = new("abc");
+            // regular expression for exactly any 5 characters - ^.{5}$
+            // regular expression which start with character 'h' and then has exactly 4 characters - ^h.{4}$
+            Regex re = new("^h.{4}$");
             foreach (var line in lines)
             {
                 // similar to 'line.Contains("abc")'

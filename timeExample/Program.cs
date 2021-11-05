6dbc8a2df522205885b51d53ea59db24a749bfd5
MinisX <daniel.dedoukh@gmail.com>
2021-11-05 15:44:34 +0100

    how to measure elapsed time

---
+++ timeExample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace timeExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime start;
+            DateTime end;
+            System.Console.WriteLine("start");
+            start = DateTime.Now;
+            Thread.Sleep(1000);
+            end = DateTime.Now;
+            System.Console.WriteLine("stop");
+            System.Console.WriteLine($"time elapsed = {end-start}");
+        }
+    }
+}

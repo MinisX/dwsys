25e363697383e2549b7c95c3866921a46d34e883
MinisX <daniel.dedoukh@gmail.com>
2021-11-05 17:18:35 +0100

    dependency ejection example 2

--- depinj/Program.cs
+++ depinj/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.Extensions.DependencyInjection;
 
 // Dependency injection is used when we create a web application
 namespace depinj
@@ -15,12 +16,24 @@ namespace depinj
         }
     }
 
+    interface IStartUp{
+        void startUp(String m);
+    }
+
+    class StartUp : IStartUp
+    {
+        public void startUp(string m)
+        {
+            System.Console.WriteLine("STARTUP " + m);
+        }
+    }
     // we assume this is web server
     // we depdent on interface ILogger and not implementation ( Logger )
     class App{
         ILogger l;
 
-        public App(ILogger l){
+        public App(ILogger l, IStartUp s){
+            s.startUp("MyApp");
             this.l = l;
         }
 
@@ -33,8 +46,19 @@ namespace depinj
     {
         static void Main(string[] args)
         {
-            Logger mylog = new Logger();
-            (new App(mylog)).run();
+            // we have to include package using terminal ' dotnet add package Microsoft.Extensions.DependencyInjection '
+            var services = new ServiceCollection();
+            // now we register our services into container
+            // now the services know that there is a service which exposes interface ILogger and actual implenetation is Logger class
+            services.AddTransient<ILogger, Logger>();
+            // register another service we defined
+            services.AddTransient<IStartUp, StartUp>();
+            // register our app
+            services.AddTransient<App>();
+            // this now creates our container based on registered services
+            ServiceProvider p = services.BuildServiceProvider();
+            App app = p.GetService<App>();
+            app.run();
         }
     }
 }

c00e80b6117de240012a858e49a590e0d20f100d
MinisX <daniel.dedoukh@gmail.com>
2021-11-05 16:57:15 +0100

    dependency injection example 1

---
+++ depinj/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+// Dependency injection is used when we create a web application
+namespace depinj
+{
+
+    interface ILogger{
+        void print(String message)
+;    }
+
+    // we assume this is loggin device
+    class Logger : ILogger{
+        public void print(String message){
+            System.Console.WriteLine(message);
+        }
+    }
+
+    // we assume this is web server
+    // we depdent on interface ILogger and not implementation ( Logger )
+    class App{
+        ILogger l;
+
+        public App(ILogger l){
+            this.l = l;
+        }
+
+        public void run(){
+            l.print("startup");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger mylog = new Logger();
+            (new App(mylog)).run();
+        }
+    }
+}
